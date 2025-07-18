773321a2b9586130ff5c4fc9734c95e9567ae2d5
ZHIIER <zhiiernga@gmail.com>
2025-07-18 21:02:24 +0800

    zhiier

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # Load the dataset
-df = pd.read_csv('AI_Assignment/input/winequality-white.csv')
-
+df = pd.read_csv('AI_Assignment/input/winequality-white.csv', sep=';', na_values=['null', 'Null', 'None', 'none', ' '])
+# drop the quality attributes, because it is a target score
+df = df.drop("quality", axis = 1)
 
 # ------------------------------------------------ Remove duplicate ------------------------------------------------
 
@@ -11,4 +13,17 @@ df = df.drop_duplicates()
 
 # ------------------------------------------------ Handle Missing Value ------------------------------------------------
 
-print(df.isnull().sum())
+#drop the row with the missing value
+df.dropna(inplace=True)
+
+# ------------------------------------------------ Normalized data ------------------------------------------------
+
+# Initialize MinMaxScaler
+scaler = MinMaxScaler()
+
+# Apply scaler to df
+normalized_data = scaler.fit_transform(df)
+
+# Create a new Data Frame
+normalized_df = pd.DataFrame(normalized_data, columns=df.columns)
+
