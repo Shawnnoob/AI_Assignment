6f4ed883176750878f62c9413cce037419ae059a
ZHIIER <zhiiernga@gmail.com>
2025-07-26 21:27:18 +0800

    zi

--- Data_Preparation/Data_Exploration.py
+++ Data_Preparation/Data_Exploration.py
@@ -5,9 +5,9 @@ import seaborn as sns # Data Visualization
 pd.set_option('display.max_columns', None)
 #pd.set_option('display.max_rows', None)
 
-ori_df = pd.read_csv("../input/clean_df.csv", delimiter=',')
+ori_df = pd.read_csv("AI_Assignment/input/clean_df.csv", delimiter=',')
 df = ori_df.copy()
-df2 = pd.read_csv("../input/normalized_df.csv", delimiter=',')
+df2 = pd.read_csv("AI_Assignment/input/normalized_df.csv", delimiter=',')
 
 # Display dataframe info
 df.info()
@@ -17,49 +17,49 @@ df2.info()
 print(df2.describe())
 
 fig, axes = plt.subplots(1, 2, figsize = (30, 10)) # 1st plot
-sns.histplot(ax = axes[0], x = df["fixed_acidity"],
+sns.histplot(ax = axes[0], x = df["Fixed_Acidity"],
              bins = 10,
              kde = True,
              cbar = True,
              color = "#CA96EC").set(title = "Distribution of 'fixed_acidity'")
 
-sns.histplot(ax = axes[1], x = df["volatile_acidity"],
+sns.histplot(ax = axes[1], x = df["Volatile_Acidity"],
              bins = 10,
              cbar = True,
              kde = True,
              color = "#A163CF").set(title = "Distribution of 'volatile_acidity'")
-plt.savefig("../graphs/dist_FixedAcidity_VolatileAcidity.png", dpi=300) # Saves 1st plot as png
+plt.savefig("AI_Assignment/graphs/dist_FixedAcidity_VolatileAcidity.png", dpi=300) # Saves 1st plot as png
 
 fig, axes = plt.subplots(1, 2, figsize=(30, 10)) # 2nd plot
-sns.histplot(ax = axes[0], x = df["citric_acid"],
+sns.histplot(ax = axes[0], x = df["Citric_Acid"],
              bins = 10,
              kde = True,
              cbar = True,
              color = "#29066B").set(title = "Distribution of 'citric_acid'")
 
-sns.histplot(ax = axes[1], x = df["alcohol"],
+sns.histplot(ax = axes[1], x = df["Alcohol"],
              bins = 10,
              kde = True,
              cbar = True,
              color = "#641811").set(title = "Distribution of 'alcohol'")
-plt.savefig("../graphs/dist_CitricAcid_Alcohol.png", dpi=300) # Saves 2nd plot as png
+plt.savefig("AI_Assignment/graphs/dist_CitricAcid_Alcohol.png", dpi=300) # Saves 2nd plot as png
 
 fig, axes = plt.subplots(1, 2, figsize=(30, 10)) # 3rd plot
-sns.histplot(ax = axes[0], x = df["residual_sugar"],
+sns.histplot(ax = axes[0], x = df["Residual_Sugar"],
              bins = 10,
              kde = True,
              cbar = True,
              color = "#EB548C").set(title = "Distribution of 'residual_sugar'")
 
-sns.histplot(ax = axes[1], x = df["chlorides"],
+sns.histplot(ax = axes[1], x = df["Chlorides"],
              bins = 10,
              kde = True,
              cbar = True,
              color = "#EC96E0").set(title = "Distribution of 'chlorides'")
-plt.savefig("../graphs/dist_ResidualSugar_Chlorides.png", dpi=300) # Saves 3rd plot as png
+plt.savefig("AI_Assignment/graphs/dist_ResidualSugar_Chlorides.png", dpi=300) # Saves 3rd plot as png
 
 # Correlation Heatmap
-corr_matrix = df2.corr(numeric_only=True)
+corr_matrix = df.corr(numeric_only=True)
 plt.figure(figsize=(15, 15))
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5).set(title = "Correlation Matrix")
-plt.savefig("../graphs/Correlation_Matrix.png", dpi=300)
+plt.savefig("AI_Assignment/graphs/Correlation_Matrix.png", dpi=300)
---
+++ Data_Preparation/Data_Preprocessing.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import LabelEncoder
+
+pd.set_option('display.max_columns', None)
+
+# Load the dataset
+df = pd.read_csv('AI_Assignment/input/winequality-white.csv', sep = ';', na_values = ['null', 'Null', 'None', 'none', ' '])
+
+# rename all the attributes
+df.rename(columns = {"fixed acidity": "Fixed_Acidity", "volatile acidity": "Volatile_Acidity",
+                     "citric acid": "Citric_Acid", "residual sugar": "Residual_Sugar",
+                     "chlorides": "Chlorides", "free sulfur dioxide": "Free_Sulfur_Dioxide",
+                     "total sulfur dioxide": "Total_Sulfur_Dioxide", "alcohol": "Alcohol", 
+                     "sulphates" : "Sulphates"}, inplace = True)
+
+
+
+
+# --------------------------------------- Generate New Attributes for Quality ---------------------------------------
+
+def map_quality_label(q):
+    if q <= 4:
+        return 'Low' # if quality is small than 4 is low quality
+    elif q <= 7:
+        return 'Normal'# if quality is small or equal than 6 is normal quality
+    else:
+        return 'High'# if quality is large than 6 is high quality
+
+df['Quality'] = df['quality'].apply(map_quality_label)
+
+
+
+
+# --------------------------------------- Remove duplicate and Missing Value ---------------------------------------
+
+# drop the duplicate row
+df = df.drop_duplicates()
+
+# drop the row with the missing value
+df.dropna(inplace = True)
+
+# Save a copy of clean dataset for backup and data exploration purpose
+df.to_csv("AI_Assignment/input/clean_df.csv", index = False)
+
+
+
+
+# --------------------------------------- Mixed the Attribute ---------------------------------------
+
+# since the Residual_Sugar and density have the high correlation, so i mixed it
+df["Sugar_Density"] = df["Residual_Sugar"] * df["density"]
+
+df = df.drop(['quality', 'Residual_Sugar', 'density'], axis = 1)
+
+df.to_csv("AI_Assignment/input/mix_df.csv", index = False)
+
+
+
+
+# --------------------------------------- Normalized data ---------------------------------------
+
+# drop the quality first because string cannot normalized
+Nor_df = df.drop("Quality", axis = 1)
+
+# Initialize MinMaxScaler
+scaler = MinMaxScaler()
+
+# Apply scaler to df
+normalized_data = scaler.fit_transform(Nor_df)
+
+# Create a new normalized Data Frame
+normalized_df = pd.DataFrame(normalized_data, columns = Nor_df.columns)
+
+#print(normalized_df)
+normalized_df.to_csv("AI_Assignment/input/normalized_df.csv", index=False)
+
+
+
+
+# --------------------------------------- Split into training and test set and balancing  ---------------------------------------
+
+# get the type of value of the quality then encode in numerical
+labels = df['Quality'].values
+
+le = LabelEncoder()
+y = le.fit_transform(labels)  # to become 0, 1, 2
+X = normalized_df
+
+# split the dataset into training set = 80% and testing set = 20%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+#oversampling with balancing the data
+sm = SMOTE(random_state=42)
+X_train_balanced, y_train_balanced = sm.fit_resample(X_train, y_train)
+
